8f9fe449430561c6833949ac2d3a262dcdca9de5
VEDA95 <snetterfield1@gmail.com>
2025-08-09 22:54:39 -0400

    Implemented tables and foreign keys for kanban boards...

---
+++ migrations/000002_board.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS open_board_workspace CASCADE;
+DROP TABLE IF EXISTS open_board_board CASCADE;
+DROP TABLE IF EXISTS open_board_board_list CASCADE;
+DROP TABLE IF EXISTS open_board_board_label CASCADE;
+DROP TABLE IF EXISTS open_board_board_field CASCADE;
+DROP TABLE IF EXISTS open_board_workspace_permissions CASCADE;
+DROP TABLE IF EXISTS open_board_board_permissions CASCADE;
