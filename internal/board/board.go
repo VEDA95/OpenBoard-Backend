009db2f77b1e99d361d8e83761630aa56faa2308
VEDA95 <snetterfield1@gmail.com>
2025-08-13 22:47:30 -0400

    WIP finidhed writing initial implementation of query functions for workspace and board data...

--- internal/board/board.go
+++ internal/board/board.go
@@ -88,13 +88,20 @@ func BoardsQuery() *sqlbuilder.SelectBuilder {
 		Join("open_board_user", "open_board_board.user_id = open_board_user.id")
 }
 
-func GetWorkspaces() ([]Workspace, error) {
+func WorkspacePerimssionsQuery() *sqlbuilder.SelectBuilder {
+	return sqlbuilder.Select("open_board_role_permission.id AS permission_id", "open_board_role_permission.path AS permission_path").
+		From("open_board_workspace_permissions").
+		Join("open_board_workspace", "open_board_workspace_permissions.workspace_id = open_board_workspace.id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permission", "open_board_workspace_permissions.permission_id = open_board_role_permission.id")
+}
+
+func GetWorkspaces() ([]*Workspace, error) {
 	if db.Instance == nil {
-		return nil, errors.New("Database not initialized")
+		return nil, errors.New("database not initialized")
 	}
 
 	workspaceQuery := WorkspaceQuery()
-	workspaces := make([]Workspace, 0)
+	workspaces := make([]*Workspace, 0)
 
 	if err := db.Instance.Many(workspaceQuery, workspaces); err != nil {
 		return nil, err
@@ -143,14 +150,14 @@ func GetWorkspaces() ([]Workspace, error) {
 	}
 
 	auth.AppendRolesToUsers(userRows, &users)
-	AppendPermissionsToWorkspaces(workspaceRows, &workspaces)
+	AppendPermissionsToWorkspaces(workspaceRows, workspaces)
 
 	return workspaces, nil
 }
 
 func GetWorkspace(ID string) (*Workspace, error) {
 	if db.Instance == nil {
-		return nil, errors.New("Database not initialized")
+		return nil, errors.New("database not initialized")
 	}
 
 	workspaceQuery := WorkspaceQuery()
@@ -164,17 +171,11 @@ func GetWorkspace(ID string) (*Workspace, error) {
 
 	workspaceRows := make([]map[string]any, 0)
 	userRows := make([]map[string]any, 0)
-	workspacePermissionQuery := sqlbuilder.Select(
-		"open_board_role_permission.id AS permission_id",
-		"open_board_role_permission.path AS permission_path",
-	).From("open_board_workspace_permissions")
+	workspacePermissionQuery := WorkspacePerimssionsQuery()
 	usersRolesQuery := auth.UsersRolesQuery()
 
 	usersRolesQuery.Where(usersRolesQuery.Equal("open_board_user_roles.user_id", workspace.User.Id))
-	workspacePermissionQuery.
-		Join("open_board_workspace", "open_board_workspace_permissions.workspace_id = open_board_workspace.id").
-		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permission", "open_board_workspace_permissions.permission_id = open_board_role_permission.id").
-		Where(workspacePermissionQuery.Equal("open_board_workspace_permissions.workspace_id", ID))
+	workspacePermissionQuery.Where(workspacePermissionQuery.Equal("open_board_workspace_permissions.workspace_id", ID))
 
 	if err := db.Instance.Many(workspacePermissionQuery, workspaceRows); err != nil {
 		return nil, err
@@ -190,11 +191,161 @@ func GetWorkspace(ID string) (*Workspace, error) {
 	return workspace, nil
 }
 
-func GetBoards() []Board {}
+func GetBoards() ([]Board, error) {
+	if db.Instance == nil {
+		return nil, errors.New("database not initialized")
+	}
 
-func GetBoard(ID string) *Board {}
+	boardQuery := BoardsQuery()
+	boards := make([]Board, 0)
 
-func AppendPermissionsToWorkspaces(rows []map[string]any, workspaces *[]Workspace) {
+	if err := db.Instance.Many(boardQuery, &boards); err != nil {
+		return nil, err
+	}
+
+	workspaceIds := make([]any, len(boards))
+	userIds := make([]any, len(boards))
+	workspaceUserIds := make([]any, len(boards))
+	workspaces := make([]*Workspace, len(boards))
+	users := make([]*auth.User, len(boards))
+	workspaceUsers := make([]*auth.User, len(boards))
+
+	for index, board := range boards {
+		workspaceIds[index] = board.Workspace.ID
+		userIds[index] = board.User.Id
+		workspaceUserIds[index] = board.Workspace.User.Id
+		workspaces[index] = board.Workspace
+		users[index] = board.User
+		workspaceUsers[index] = board.Workspace.User
+	}
+
+	workspaceRows := make([]map[string]any, 0)
+	userRows := make([]map[string]any, 0)
+	workspaceUserRows := make([]map[string]any, 0)
+	workspacePermissionQuery := sqlbuilder.Select(
+		"open_board_workspace.id AS workspace_id",
+		"open_board_role_permission.id AS permission_id",
+		"open_board_role_permission.path AS permission_path",
+	).From("open_board_workspace_permissions")
+	usersRolesQuery := sqlbuilder.Select(
+		"open_board_user_roles.user_id",
+		"open_board_role.id AS role_identifier",
+		"open_board_role.name AS role_name",
+		"open_board_role_permission.id AS permission_identifier",
+		"open_board_role_permission.path AS permission_path",
+	).From("open_board_user_roles")
+	workspaceUserRolesQuery := sqlbuilder.Select(
+		"open_board_user_roles.user_id",
+		"open_board_role.id AS role_identifier",
+		"open_board_role.name AS role_name",
+		"open_board_role_permission.id AS permission_identifier",
+		"open_board_role_permission.path AS permission_path",
+	).From("open_board_user_roles")
+
+	usersRolesQuery.
+		Join("open_board_role", "open_board_user_roles.role_id = open_board_role.id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permissions", "open_board_role.id = open_board_role_permissions.role_id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permission", "open_board_role_permissions.permission_id = open_board_role_permission.id").
+		Where(usersRolesQuery.In("open_board_user_roles.user_id", userIds...))
+	workspaceUserRolesQuery.
+		Join("open_board_role", "open_board_user_roles.role_id = open_board_role.id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permissions", "open_board_role.id = open_board_role_permissions.role_id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permission", "open_board_role_permissions.permission_id = open_board_role_permission.id").
+		Where(usersRolesQuery.In("open_board_user_roles.user_id", workspaceUserIds...))
+	workspacePermissionQuery.Where(workspacePermissionQuery.In("open_board_workspace_permissions.workspace_id", workspaceIds...)).
+		Join("open_board_workspace", "open_board_workspace_permissions.workspace_id = open_board_workspace.id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permission", "open_board_workspace_permissions.permission_id = open_board_role_permission.id")
+
+	if err := db.Instance.Many(workspacePermissionQuery, workspaceRows); err != nil {
+		return nil, err
+	}
+
+	if err := db.Instance.Many(usersRolesQuery, userRows); err != nil {
+		return nil, err
+	}
+
+	if err := db.Instance.Many(workspaceUserRolesQuery, workspaceUserRows); err != nil {
+		return nil, err
+	}
+
+	auth.AppendRolesToUsers(userRows, &users)
+	auth.AppendRolesToUsers(workspaceUserRows, &workspaceUsers)
+	AppendPermissionsToWorkspaces(workspaceRows, workspaces)
+
+	return boards, nil
+}
+
+func GetBoard(ID string) (*Board, error) {
+	if db.Instance == nil {
+		return nil, errors.New("database not initialized")
+	}
+
+	boardQuery := BoardsQuery()
+	board := new(Board)
+
+	boardQuery.Where(boardQuery.Equal("id", ID))
+
+	if err := db.Instance.One(boardQuery, board); err != nil {
+		return nil, err
+	}
+
+	workspaceRows := make([]map[string]any, 0)
+	userRows := make([]map[string]any, 0)
+	workspaceUserRows := make([]map[string]any, 0)
+	workspacePermissionQuery := sqlbuilder.Select(
+		"open_board_workspace.id AS workspace_id",
+		"open_board_role_permission.id AS permission_id",
+		"open_board_role_permission.path AS permission_path",
+	).From("open_board_workspace_permissions")
+	usersRolesQuery := sqlbuilder.Select(
+		"open_board_user_roles.user_id",
+		"open_board_role.id AS role_identifier",
+		"open_board_role.name AS role_name",
+		"open_board_role_permission.id AS permission_identifier",
+		"open_board_role_permission.path AS permission_path",
+	).From("open_board_user_roles")
+	workspaceUserRolesQuery := sqlbuilder.Select(
+		"open_board_user_roles.user_id",
+		"open_board_role.id AS role_identifier",
+		"open_board_role.name AS role_name",
+		"open_board_role_permission.id AS permission_identifier",
+		"open_board_role_permission.path AS permission_path",
+	).From("open_board_user_roles")
+
+	usersRolesQuery.
+		Join("open_board_role", "open_board_user_roles.role_id = open_board_role.id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permissions", "open_board_role.id = open_board_role_permissions.role_id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permission", "open_board_role_permissions.permission_id = open_board_role_permission.id").
+		Where(usersRolesQuery.Equal("open_board_user_roles.user_id", board.User.Id))
+	workspaceUserRolesQuery.
+		Join("open_board_role", "open_board_user_roles.role_id = open_board_role.id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permissions", "open_board_role.id = open_board_role_permissions.role_id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permission", "open_board_role_permissions.permission_id = open_board_role_permission.id").
+		Where(usersRolesQuery.Equal("open_board_user_roles.user_id", board.Workspace.User.Id))
+	workspacePermissionQuery.Where(workspacePermissionQuery.Equal("open_board_workspace_permissions.workspace_id", board.Workspace.ID)).
+		Join("open_board_workspace", "open_board_workspace_permissions.workspace_id = open_board_workspace.id").
+		JoinWithOption(sqlbuilder.LeftJoin, "open_board_role_permission", "open_board_workspace_permissions.permission_id = open_board_role_permission.id")
+
+	if err := db.Instance.Many(workspacePermissionQuery, workspaceRows); err != nil {
+		return nil, err
+	}
+
+	if err := db.Instance.Many(usersRolesQuery, userRows); err != nil {
+		return nil, err
+	}
+
+	if err := db.Instance.Many(workspaceUserRolesQuery, workspaceUserRows); err != nil {
+		return nil, err
+	}
+
+	auth.AppendRolesToUser(userRows, board.User)
+	auth.AppendRolesToUser(workspaceUserRows, board.Workspace.User)
+	AppendPermissionsToWorkspace(workspaceRows, board.Workspace)
+
+	return board, nil
+}
+
+func AppendPermissionsToWorkspaces(rows []map[string]any, workspaces []*Workspace) {
 	permissionMap := make(map[string]auth.RolePermission)
 
 	for _, row := range rows {
@@ -208,16 +359,16 @@ func AppendPermissionsToWorkspaces(rows []map[string]any, workspaces *[]Workspac
 		}
 	}
 
-	for index := range *workspaces {
+	for index := range workspaces {
 		permissions := make([]auth.RolePermission, 0)
 
 		for _, row := range rows {
-			if row["workspace_id"].(uuid.UUID).String() == (*workspaces)[index].ID {
+			if row["workspace_id"].(uuid.UUID).String() == workspaces[index].ID {
 				permissions = append(permissions, permissionMap[row["permission_id"].(uuid.UUID).String()])
 			}
 		}
 
-		(*workspaces)[index].Permissions = permissions
+		workspaces[index].Permissions = permissions
 	}
 }
 
