24f1f7085ab9448ade11a713dd2a29d11202c13c
VEDA95 <snetterfield1@gmail.com>
2025-08-23 02:25:19 -0400

    WIP refactored entrypoint to initialize repositories, services, and handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	_ "VEDA95/open_board/api/docs"
 	"VEDA95/open_board/api/internal/config"
 	"VEDA95/open_board/api/internal/db"
+	models "VEDA95/open_board/api/internal/db/model"
 	"VEDA95/open_board/api/internal/db/repository"
 	"VEDA95/open_board/api/internal/errors"
 	"VEDA95/open_board/api/internal/http/middleware"
@@ -33,12 +34,34 @@ func main() {
 		log.Fatal(err)
 	}
 
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		log.Fatal("environment variable PORT is not set")
+	}
+
+	host := os.Getenv("HOST")
+	var hostString string
+
+	if len(host) == 0 {
+		hostString = fmt.Sprintf("127.0.0.1:%s", port)
+	} else if host == "0.0.0.0" {
+		hostString = fmt.Sprintf(":%s", port)
+	} else {
+		hostString = fmt.Sprintf("%s:%s", host, port)
+	}
+
 	logger, err := applogger.NewLogger()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	models := []any{}
+	models := []any{
+		models.User{},
+		models.Session{},
+		models.Role{},
+		models.Permission{},
+		models.PasswordResetToken{},
+	}
 	dbInstance, err := db.NewDB(logger, models)
 	if err != nil {
 		logger.Fatal().Err(err).Msg("")
@@ -58,23 +81,20 @@ func main() {
 		log.Print("WARN:", err)
 	}
 
+	userRepo := repository.NewUserRepository(dbInstance)
+	sessionRepo := repository.NewSessionRepository(dbInstance)
+	roleRepo := repository.NewRoleRepository(dbInstance)
+	permissionRepo := repository.NewPermissionRepository(dbInstance)
+	passwordResetRepo := repository.NewPasswordResetRepository(dbInstance)
+	userService := service.NewUserService(userRepo, roleRepo)
+	authService := service.NewAuthService(sessionRepo, userRepo, passwordResetRepo)
+	roleService := service.NewRoleService(roleRepo)
+	permissionService := service.NewPermissionService(permissionRepo)
 	validator := validators.NewValidator()
-	port := os.Getenv("PORT")
-
-	if len(port) == 0 {
-		logger.Fatal().Msg("environment variable PORT is not set")
-	}
-
-	host := os.Getenv("HOST")
-	var hostString string
-
-	if len(host) == 0 {
-		hostString = fmt.Sprintf("127.0.0.1:%s", port)
-	} else if host == "0.0.0.0" {
-		hostString = fmt.Sprintf(":%s", port)
-	} else {
-		hostString = fmt.Sprintf("%s:%s", host, port)
-	}
+	userHandler := routes.NewUserHandler(userService, validator)
+	authHandler := routes.NewAuthHandler(authService, userService, emailService, validator)
+	roleHandler := routes.NewRoleHandler(roleService, validator)
+	permissionHandler := routes.NewPermissionHandler(permissionService, validator)
 
 	app := fiber.New(fiber.Config{
 		ErrorHandler: errors.ErrorHandler,
@@ -92,35 +112,33 @@ func main() {
 		AllowCredentials: true,
 	}))
 	app.Get("/swagger/*", swagger.HandlerDefault)
-	authGroup.Post("/login", routes.LocalLogin)
-	authGroup.Post("/refresh", routes.LocalRefresh)
-	authGroup.Post("/logout/:id", middleware.CheckUserAuthentication, routes.LocalLogoutById)
-	authGroup.Post("/logout", middleware.CheckUserAuthentication, routes.LocalLogout)
-	authGroup.Post("/password_reset", middleware.CheckUserAuthentication, routes.LocalAuthenticatedPasswordReset)
-	authGroup.Post("/password_reset/token", middleware.CheckUserAuthentication, routes.LocalAuthenticatedPasswordTokenIssuer)
-	authGroup.Post("/forgot_password", routes.LocalUnauthenticatedPasswordReset)
-	authGroup.Post("/forgot_password/token", routes.LocalUnauthenticatedPasswordTokenIssuer)
-	authGroup.Post("/forgot_password/token/introspect", routes.LocalUnauthenticatedPasswordResetTokenIntrospect)
-	authGroup.Get("/@me", middleware.CheckUserAuthentication, routes.UserInfoGET)
-	authGroup.Patch("/@me", middleware.CheckUserAuthentication, routes.UserInfoPATCH)
-	authGroup.Delete("/@me", middleware.CheckUserAuthentication, routes.UserInfoDELETE)
-	authGroup.Get("/@me/sessions", middleware.CheckUserAuthentication, routes.UserSessionsGET)
-	authGroup.Get("/roles", routes.RolesGET)
-	authGroup.Post("/roles", routes.RolesPOST)
-	authGroup.Get("/roles/:id", routes.RoleGET)
-	authGroup.Patch("/roles/:id", routes.RolePATCH)
-	authGroup.Delete("/roles/:id", routes.RoleDELETE)
-	authGroup.Get("/permissions", routes.PermissionsGET)
-	authGroup.Post("/permissions", routes.PermissionsPOST)
-	authGroup.Get("/permissions/:id", routes.PermissionGET)
-	authGroup.Patch("/permissions/:id", routes.PermissionPATCH)
-	authGroup.Delete("/permissions/:id", routes.PermissionDELETE)
-	apiGroup.Get("/", routes.IndexGET)
-	apiGroup.Get("/users", routes.UsersGET)
-	apiGroup.Post("/users", routes.UsersPOST)
-	apiGroup.Get("/users/:id", routes.UserGET)
-	apiGroup.Patch("/users/:id", routes.UserPATCH)
-	apiGroup.Delete("/users/:id", routes.UserDELETE)
+	authGroup.Post("/login", authHandler.LocalLogin)
+	authGroup.Post("/refresh", authHandler.LocalRefresh)
+	authGroup.Post("/logout/:id", middleware.CheckUserAuthentication, authHandler.LocalLogoutById)
+	authGroup.Post("/logout", middleware.CheckUserAuthentication, authHandler.LocalLogout)
+	authGroup.Post("/password_reset", middleware.CheckUserAuthentication, authHandler.LocalAuthenticatedPasswordReset)
+	authGroup.Post("/password_reset/token", middleware.CheckUserAuthentication, authHandler.LocalAuthenticatedPasswordTokenIssuer)
+	authGroup.Post("/forgot_password", authHandler.LocalUnauthenticatedPasswordReset)
+	authGroup.Post("/forgot_password/token", authHandler.LocalUnauthenticatedPasswordTokenIssuer)
+	authGroup.Get("/@me", middleware.CheckUserAuthentication, authHandler.UserInfoGET)
+	authGroup.Patch("/@me", middleware.CheckUserAuthentication, authHandler.UserInfoPATCH)
+	authGroup.Delete("/@me", middleware.CheckUserAuthentication, authHandler.UserInfoDELETE)
+	authGroup.Get("/@me/sessions", middleware.CheckUserAuthentication, authHandler.UserSessionsGET)
+	authGroup.Get("/roles", roleHandler.GET)
+	authGroup.Post("/roles", roleHandler.POST)
+	authGroup.Get("/roles/:id", roleHandler.GETByID)
+	authGroup.Patch("/roles/:id", roleHandler.PATCH)
+	authGroup.Delete("/roles/:id", roleHandler.DELETE)
+	authGroup.Get("/permissions", permissionHandler.GET)
+	authGroup.Post("/permissions", permissionHandler.POST)
+	authGroup.Get("/permissions/:id", permissionHandler.GETByID)
+	authGroup.Patch("/permissions/:id", permissionHandler.PATCH)
+	authGroup.Delete("/permissions/:id", permissionHandler.DELETE)
+	apiGroup.Get("/users", userHandler.GET)
+	apiGroup.Post("/users", userHandler.POST)
+	apiGroup.Get("/users/:id", userHandler.GETByID)
+	apiGroup.Patch("/users/:id", userHandler.PATCH)
+	apiGroup.Delete("/users/:id", userHandler.DELETE)
 
 	if err := app.Listen(hostString); err != nil {
 		logger.Fatal().Err(err).Msg("Error occurred while running the server")
