c98f6bd66615ab45e966a316e9c8881996555e2a
VEDA95 <snetterfield1@gmail.com>
2025-12-18 21:19:13 -0500

    Refactored settings tables to be more focused

--- internal/db/model/authsettings.go
+++ internal/db/model/authsettings.go
@@ -15,14 +15,13 @@ type AuthSettings struct {
 	DefaultUserRole          string     `gorm:"type:varchar(50);default:'user'" json:"default_user_role"`
 	RegistrationWelcomeEmail bool       `gorm:"default:true" json:"registration_welcome_email"`
 	AllowUserInvitations     bool       `gorm:"default:true" json:"allow_user_invitations"`
-	InvitationExpiry         int        `gorm:"default:7" json:"invitation_expiry"`
+	InvitationExpiry         int        `gorm:"default:604800" json:"invitation_expiry"`
 	InviteOnlyMode           bool       `gorm:"default:false" json:"invite_only_mode"`
-	SessionTimeout           int        `gorm:"default:10080" json:"session_timeout"`
-	SessionIdleTimeout       int        `gorm:"default:60" json:"session_idle_timeout"`
-	RefreshTokenExpiry       int        `gorm:"default:30" json:"refresh_token_expiry"`
-	RememberMeDuration       int        `gorm:"default:30" json:"remember_me_duration"`
+	SessionTimeout           int        `gorm:"default:3600" json:"session_timeout"`
+	SessionIdleTimeout       int        `gorm:"default:1800" json:"session_idle_timeout"`
+	RememberMeDuration       int        `gorm:"default:86400" json:"remember_me_duration"`
 	MaxLoginAttempts         int        `gorm:"default:5" json:"max_login_attempts"`
-	LockoutDuration          int        `gorm:"default:30" json:"lockout_duration"`
+	LockoutDuration          int        `gorm:"default:18000" json:"lockout_duration"`
 	TwoFactorAuthentication  bool       `gorm:"default:false" json:"two_factor_authentication"`
 	TwoFactorRequired        bool       `gorm:"default:false" json:"two_factor_required"`
 	EnableOAuth              bool       `gorm:"default:false" json:"enable_oauth"`
--- internal/db/model/emailsettings.go
+++ internal/db/model/emailsettings.go
@@ -13,7 +13,6 @@ type EmailSettings struct {
 	EmailEnabled             bool       `gorm:"default:true" json:"email_enabled"`
 	EmailFromAddress         *string    `gorm:"type:varchar(255)" json:"email_from_address"`
 	EmailFromName            *string    `gorm:"type:varchar(100)" json:"email_from_name"`
-	EmailReplyTo             *string    `gorm:"type:varchar(255)" json:"email_reply_to"`
 	SMTPHost                 *string    `gorm:"type:varchar(255)" json:"smtp_host"`
 	SMTPPort                 int        `gorm:"default:587" json:"smtp_port"`
 	SMTPUsername             *string    `gorm:"type:varchar(255)" json:"smtp_username"`
@@ -21,8 +20,6 @@ type EmailSettings struct {
 	SMTPEncryption           *string    `gorm:"type:varchar(10);default:'tls'" json:"smtp_encryption"`    // tls, ssl, none
 	SMTPAuthMethod           *string    `gorm:"type:varchar(20);default:'plain'" json:"smtp_auth_method"` // plain, login, cram-md5
 	SMTPVerifySSL            bool       `gorm:"default:true" json:"smtp_verify_ssl"`
-	SMTPTimeout              int        `gorm:"default:30" json:"smtp_timeout"` // seconds
-	SMTPPoolSize             int        `gorm:"default:10" json:"smtp_pool_size"`
 	SendGridAPIKey           *string    `gorm:"type:varchar(255)" json:"sendgrid_api_key"`
 	MailgunAPIKey            *string    `gorm:"type:varchar(255)" json:"mailgun_api_key"`
 	MailgunDomain            *string    `gorm:"type:varchar(255)" json:"mailgun_domain"`
